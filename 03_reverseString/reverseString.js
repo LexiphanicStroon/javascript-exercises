685499de99020ccd94778591d9c1136a8b4de9b3
LexiphanicStroon <nate_reeves@outlook.com>
2023-09-20 11:17:56 -0500

    Some problem solutions

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const stringArray = string.split('');
+    const reversed = stringArray.reverse();
+    console.log(reversed);
+    const joinedReversed = reversed.join('');
+    return joinedReversed;
+    // return string.split("").reverse().join(""); elegant solution
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...items) {
+    const newArray = [];
+    array.forEach((item) => {
+        if (!items.includes(item)) {
+            newArray.push(item);
+        }
+    });
+    return newArray;
 };
 
 // Do not edit below this line
